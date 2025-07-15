de84f7b6e69c909ac8b9a64d5300d7f7c4d8b8f3
Sahil Thakur <90372772+realsahilthakur@users.noreply.github.com>
2025-07-15 15:23:25 -0400

    Update time.test.js

--- Frontend/tests/time.test.js
+++ Frontend/tests/time.test.js
@@ -1,8 +1,12 @@
 const { updateTime } = require('../time.js');
 
 describe('Time Functions', () => {
-  it('should update datetime element with current time', () => {
+  beforeEach(() => {
+    // Reset DOM before each test
     document.body.innerHTML = '<span id="datetime"></span>';
+  });
+
+  it('should update datetime element with current time', () => {
     updateTime();
     const datetime = document.getElementById('datetime').innerHTML;
     expect(datetime).to.match(/\d{1,2}:\d{2}:\d{2} (AM|PM)/);
